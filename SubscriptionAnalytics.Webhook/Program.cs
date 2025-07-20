3d2b89321f06bbf36da41134a6def4b291bbb71c
Arya <arya@Aryas-MacBook-Pro.local>
2025-07-20 14:44:42 +0530

    chore: downgrade all projects to .NET 8, update all packages to stable 8.x, and resolve all build warnings

--- SubscriptionAnalytics.Webhook/Program.cs
+++ SubscriptionAnalytics.Webhook/Program.cs
@@ -1,17 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// No Swagger or OpenAPI for now
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
 app.UseHttpsRedirection();
 
 var summaries = new[]
